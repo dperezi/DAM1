aa099749a3376925c7c2937444ce00b2d0eeb499
dperezi <dperezi@iesch.org>
2022-06-19 12:51:38 +0200

    Añadido entorno y 1er ejercicio java

---
+++ PRO1/TEMA_001/src/tema_001/ud01_001.java
@@ -0,0 +1,12 @@
+package tema_001;
+
+public class ud01_001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 3;
+		double len = 2 * Math.PI * radius;
+		System.out.printf("La longitud de una circuferencia de radio %.2f (m) es %.2f (m).", radius, len);
+	}
+
+}
